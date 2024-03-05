b9b86b90cf9b186e6b4c3d7188ef59777e66b055
Naveen Sharma <falconhawk.nav@gmail.com>
2024-03-05 16:40:11 +0530

    first commit

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,84 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../config/generateToken");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic, ownerId } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all the Fields.");
+  }
+
+  if (!ownerId) {
+    res.status(400);
+    throw new Error("Please make sure this app runs on your provider's app id");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+    ownerId,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create new user");
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  const c1 = { _id: { $ne: req.user._id } };
+  const c2 = { ownerId: req.user._id };
+
+  const users = req.user.isUser
+    ? await User.find(keyword).find({ $and: [c1, c2] })
+    : await User.find({ _id: req.user.ownerId });
+  res.status(201).json(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
